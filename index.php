5f8ed0891047e8ffc7add527b3a481cfafcc491c
bright1331 <s6603052421135@email.kmutnb.ac.th>
2024-02-07 16:54:20 +0700

    add bootstrap follow work7
    add bootstrap follow work7

--- index.php
+++ index.php
@@ -6,11 +6,36 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Webboard BXB</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard BXB</h1>
+    <div class="container-lg">
+    <h1 style="text-align: center;" class="mt-3">Webboard BXB</h1>
     <hr>
+    <nav class="navbar navbar-expand-lg " style="background-color: #d3d3d3;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i> Home</a>
+      <ul class="navbar-nav">
+        <?php if(!isset($_SESSION['id'])){?>
+        <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="login.php"><i class="bi bi-pencil-square"></i> เข้าสู่ระบบ</a>
+        </li>
+        <?php }else{?>
+            <li class="nav-item dropdown">
+          <a class="btn  btn-outline-secondary btn-sm dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-person-lines-fill"></i> <?php echo $_SESSION['username']; ?>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="logout.php"><i class="bi bi-power"></i> ออกจากระบบ</a></li>
+          </ul>
+        </li>
+        <?php } ?>
+      </ul>
+  </div>
+</nav>
     <form>
         หมวดหมู่ : 
         <select>
@@ -20,7 +45,7 @@ session_start();
         </select>
         <?php 
         if(!isset($_SESSION['id'])){
-            echo "<a href=login.html style='float: right;'>เข้าสู่ระบบ</a>";
+            echo "<a href=login.php style='float: right;'>เข้าสู่ระบบ</a>";
         }else{
             echo "<div style='float:right'>
                     ผู้ใช้งานระบบ :$_SESSION[username]&nbsp;&nbsp;
@@ -41,5 +66,6 @@ session_start();
         }
         ?>
     </ul>
+    </div>
 </body>
 </html>
